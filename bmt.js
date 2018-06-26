62e44c6542213e77be6a5934a642b4fda7523015
Fadia Rostane <39888576+FadiaR@users.noreply.github.com>
2018-06-26 02:56:11 +0200

    pleeeeasework

---
+++ bmt.js
@@ -0,0 +1,5 @@
+setTimeout(function(){ alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please"); }, 10000);
+function myFunction() {
+    var element = document.getElementById("mybody");
+    element.classList.add("mystyle");
+}
